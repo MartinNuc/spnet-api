cc39396fb343fc83da0f74480b22f90c8f9458f3
Martin Nuc <martin@nuc.cz>
2013-10-08 01:04:40 +0200

    First API version. Simply listing users and showing user detail. Basic authentication using token stored in database.

---
+++ UserModel.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once 'config.php';
+
+/**
+ * Model for users
+ */
+class UserModel
+{
+	// user detail
+	public function getUser($id)
+	{
+		$result = dibi::query('SELECT * FROM [cns_clenove] WHERE [UserID] = %i', $id);
+		return $result->fetch();
+	}
+	
+	// all users
+	public function getUsers($order = NULL, $where = NULL, $offset = NULL, $limit = NULL)
+	{
+		return dibi::query(
+		    'SELECT UserID, UserName FROM [cns_clenove] WHERE 1=1
+		     %if', isset($where), 'AND %and', isset($where) ? $where : array(), '%end',
+		    '%if', isset($order), 'ORDER BY %by', $order, '%end',
+		    '%if', isset($limit), 'LIMIT %i %end', $limit,
+		    '%if', isset($offset), 'OFFSET %i %end', $offset
+		)->fetchAll();
+	}
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,126 @@
+<?php
+require_once 'config.php';
+require_once 'vendor/autoload.php';
+require_once 'UserModel.php';
+
+\Slim\Slim::registerAutoloader();
+$app = new \Slim\Slim();
+
+// connect to database
+dibi::connect(array(
+    'driver'   => 'mysql',
+    'host'     => 'localhost',
+    'username' => 'root',
+    'password' => 'okno',
+    'database' => 'spng3',
+));
+
+// route middleware for simple API authentication
+function authenticate(\Slim\Route $route) {
+    $app = \Slim\Slim::getInstance();
+    $token = $app->getEncryptedCookie('token');
+    if (validateUserKey($token) === false) {
+      $result = array("status" => "error", "message" => 'Invalid security token.');
+      echo json_encode($result);
+      $app->stop();
+      $app->halt(401);
+    }
+}
+
+// validate user againts database
+function validateUserKey($token) {
+  $result = dibi::query('SELECT Count(*) FROM [tokens] WHERE [token] = %s', $token)->fetchSingle();
+  if ($result != null)
+  {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/************ REST API *************/
+
+// user detail
+$app->get('/user/:id', 'authenticate', function ($id) use ($app) {
+
+    $user_model = new UserModel();
+
+    $user = $user_model->getUser($id);
+
+    if ( ! $user)
+    {
+        $res = $app->response();
+        $res->status(404);
+        $res['Content-Type'] = 'application/json';
+        $res->body(json_encode(array(
+            'error' => 'User not found'
+        )));
+    }
+    else
+    {
+        // Basic response
+        $response = array(
+            'error' => null,
+            'result'    =>  array(
+                'id'   =>  $user['UserID'],
+                'username' =>  $user['UserName'],
+            )
+        );
+
+        // Respond
+        $res = $app->response();
+        $res['Content-Type'] = 'application/json';
+
+        $res->body(json_encode($response));
+    }
+});
+
+// all users
+$app->get('/user/', 'authenticate', function () use ($app) {
+
+    $user_model = new UserModel();
+
+    $users = $user_model->getUsers();
+    if ( ! $users)
+    {
+        $res = $app->response();
+        $res->status(404);
+        $res['Content-Type'] = 'application/json';
+        $res->body(json_encode(array(
+            'error' => 'Unknown error'
+        )));
+    }
+    else
+    {
+        // Basic response
+        $response = array(
+            'error' => null,
+            'result' => $users
+        );
+
+        // Respond
+        $res = $app->response();
+        $res['Content-Type'] = 'application/json';
+
+        $res->body(json_encode($response));
+    }
+});
+
+// set security token
+$app->get('/login/:token', function ($token) use ($app) {    
+  try {
+    $app->setEncryptedCookie('token', $token, '40 minutes');
+    $res = $app->response();
+    $response = array(
+        'error' => null,
+        'result' => "Token set."
+    );
+  	$res->body(json_encode($response));
+  } catch (Exception $e) {
+    $app->response()->status(400);
+    $app->response()->header('X-Status-Reason', $e->getMessage());
+  }
+});
+
+$app->run();
+?>
